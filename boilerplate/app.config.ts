fa63b1c6dd8260f12a0c9cd60e9d6f1261bfbe8b
Khadija <khadija.bencekri@theodo.com>
2024-12-09 14:07:12 +0100

    add expo secure store to app.config.ts

--- boilerplate/app.config.ts
+++ boilerplate/app.config.ts
@@ -21,6 +21,7 @@ module.exports = ({ config }: ConfigContext): Partial<ExpoConfig> => {
       ...existingPlugins,
       require("./plugins/withSplashScreen").withSplashScreen,
       require("./plugins/withFlipperDisabled").withFlipperDisabled,
+      "expo-secure-store"
     ],
   }
 }
