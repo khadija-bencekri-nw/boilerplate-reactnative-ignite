3d56c55ad38d7f1bb26eac2e7755ca24685f6606
Khadija <khadija.bencekri@theodo.com>
2024-12-16 15:26:05 +0100

    remove unused prop and styles

--- boilerplate/app/components/GoogleSignIn.tsx
+++ boilerplate/app/components/GoogleSignIn.tsx
@@ -30,7 +30,6 @@ const GoogleSignIn= forwardRef<any, GoogleSignInProps>((props, ref) => {
         style,
         preset,
         textStyle,
-        onPress,
         LeftAccessory,
         onSignInSuccess
     } = { ...defaultProps, ...props }; 
@@ -66,20 +65,3 @@ const GoogleSignIn= forwardRef<any, GoogleSignInProps>((props, ref) => {
 
 export default GoogleSignIn;
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'white'
-    },
-text: {
-        fontWeight: 'bold',
-        marginBottom: 20,
-        fontSize: 17
-    },
-googlebtn: {
-        width: 192,
-        height: 48
-    }
-})
