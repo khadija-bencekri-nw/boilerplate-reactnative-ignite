ffe058fe6d238dbddb84f30305e5e5e72951fc8f
Khadija <khadija.bencekri@theodo.com>
2024-12-23 17:29:31 +0100

    apply prettier and remove comments and unused value

--- boilerplate/app/models/Purchase.ts
+++ boilerplate/app/models/Purchase.ts
@@ -1,20 +1,9 @@
-import { Instance, SnapshotIn, SnapshotOut, types } from "mobx-state-tree"
-import { withSetPropAction } from "./helpers/withSetPropAction"
-import { formatDate } from "../utils/formatDate"
 import { translate } from "../i18n"
 
-interface Enclosure {
-  id: string
-  brand: string
-  model: string
-  store: string
-  price: number
-  purchaseDate: Date
-  rating: number
-  //rating: Object
-  userId: string
-  images: string[]
-}
+import { withSetPropAction } from "./helpers/withSetPropAction"
+
+import type { Instance, SnapshotIn, SnapshotOut } from "mobx-state-tree"
+import { types } from "mobx-state-tree"
 
 /**
  * This represents an Purchase of React Native Radio.
@@ -24,38 +13,29 @@ export const PurchaseModel = types
   .props({
     id: types.identifier,
     brand: "",
-    purchaseDate: new Date(), // Ex: 2022-08-12 21:05:36
-    model: "",
     store: "",
     price: 0,
+    purchaseDate: new Date(), // Ex: 2022-08-12 21:05:36
     rating: 0,
-    // rating: types.model({
-    //   stars: types.optional(types.number, 0), // Default stars: 0
-    //   reviews: types.optional(types.number, 0), // Default reviews: 0
-    // }),
+    model: "",
     userId: "",
     images: types.optional(types.array(types.string), []),
   })
   .actions(withSetPropAction)
-  .views((Purchase) => ({
+  .views((purchase) => ({
     get parsedTitleAndSubtitle() {
-      const defaultValue = { title: Purchase.brand?.trim(), subtitle: "" }
+      const defaultValue = { title: purchase.brand.trim(), subtitle: "" }
 
-      if (!defaultValue.title) return defaultValue
+      if (defaultValue.title.length === 0) return defaultValue
 
-      const titleMatches = defaultValue.title.match(/^(RNR.*\d)(?: - )(.*$)/)
-
-      if (!titleMatches || titleMatches.length !== 3) return defaultValue
-
-      return { title: titleMatches[1], subtitle: titleMatches[2] }
+      return { title: defaultValue.title, subtitle: defaultValue.title }
     },
     get datePublished() {
       try {
-        const formatted = formatDate(Purchase.purchaseDate)
         return {
-          textLabel: formatted,
-          accessibilityLabel: translate("demoPodcastListScreen.accessibility.publishLabel", {
-            date: formatted,
+          textLabel: purchase.purchaseDate,
+          accessibilityLabel: translate("productScreen.purchaseDate", {
+            date: purchase.purchaseDate,
           }),
         }
       } catch (error) {
