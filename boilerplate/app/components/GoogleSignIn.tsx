c8762509eb36c50d4197a252cf91cfd585ee302e
Khadija <khadija.bencekri@theodo.com>
2024-12-23 09:29:57 +0100

    apply eslint

--- boilerplate/app/components/GoogleSignIn.tsx
+++ boilerplate/app/components/GoogleSignIn.tsx
@@ -1,67 +1,66 @@
-import React, {forwardRef} from 'react';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
-import { StyleSheet } from 'react-native';
-import { Button } from './Button';
+import React, { forwardRef } from "react"
+
+import type { ButtonAccessoryProps } from "./Button"
+import { Button } from "./Button"
+
+import { GoogleSignin } from "@react-native-google-signin/google-signin"
+import type { TxKeyPath } from "app/i18n"
+import type { ComponentType } from "react"
 
 type GoogleSignInProps = {
-    testID?: string;
-    tx: string;
-    style?: object;
-    preset?: string;
-    textStyle?: object;
-    onPress?: () => void;
-    LeftAccessory?: () => void;
-    onSignInSuccess: (userInfo: any) => void;
-};
+  testID?: string
+  tx: TxKeyPath
+  style?: object
+  preset?: "default" | "filled" | "reversed" | undefined
+  textStyle?: object
+  onPress?: () => void
+  LeftAccessory?: ComponentType<ButtonAccessoryProps>
+  onSignInSuccess: (userInfo: any) => void
+}
 
 const defaultProps: Partial<GoogleSignInProps> = {
-    testID: "google-sign-in",
-    tx: "loginScreen.google",
-    style: {},
-    preset: "default",
-    textStyle: {},
-    onPress: () => console.log("GoogleSignIn button pressed"),
-    LeftAccessory: () => null,
-};
-const GoogleSignIn= forwardRef<any, GoogleSignInProps>((props, ref) => {
-    const {
-        testID,
-        tx,
-        style,
-        preset,
-        textStyle,
-        LeftAccessory,
-        onSignInSuccess
-    } = { ...defaultProps, ...props }; 
+  testID: "google-sign-in",
+  tx: "loginScreen.google",
+  style: {},
+  preset: "default",
+  textStyle: {},
+  onPress: () => {
+    console.log("GoogleSignIn button pressed")
+  },
+  LeftAccessory: () => null,
+}
+const GoogleSignIn = forwardRef<any, GoogleSignInProps>(function GoogleSignIn(props, ref) {
+  const { testID, tx, style, preset, textStyle, LeftAccessory, onSignInSuccess } = {
+    ...defaultProps,
+    ...props,
+  }
 
-    const signIn = async () => {
-        GoogleSignin.configure({
-            scopes: ['https://www.googleapis.com/auth/drive.readonly'],
-            webClientId: '777558541856-ghcru6v9c4gi4mh5i6aj7jlk95vjb9ds.apps.googleusercontent.com',
-            offlineAccess: true
-        });
-        try {
-                await GoogleSignin.hasPlayServices();
-                const userInfo = await GoogleSignin.signIn();
-                onSignInSuccess(userInfo);
-            } catch (error) {
-                console.log("error : ",error)
-        }
-    };
+  const signIn = async () => {
+    GoogleSignin.configure({
+      scopes: ["https://www.googleapis.com/auth/drive.readonly"],
+      webClientId: "777558541856-ghcru6v9c4gi4mh5i6aj7jlk95vjb9ds.apps.googleusercontent.com",
+      offlineAccess: true,
+    })
+    try {
+      await GoogleSignin.hasPlayServices()
+      const userInfo = await GoogleSignin.signIn()
+      onSignInSuccess(userInfo)
+    } catch (error) {
+      console.log("error : ", error)
+    }
+  }
 
-    return (
-        <Button
-            ref={ref}
-            testID={testID}
-            tx={tx}
-            style={style}
-            preset={preset}  
-            textStyle={textStyle}
-            onPress={() => signIn()}
-            LeftAccessory={LeftAccessory}
-        />
-    )
-});
+  return (
+    <Button
+      testID={testID}
+      tx={tx}
+      style={style}
+      preset={preset}
+      textStyle={textStyle}
+      onPress={async () => signIn()}
+      LeftAccessory={LeftAccessory}
+    />
+  )
+})
 
-export default GoogleSignIn;
-
+export default GoogleSignIn
