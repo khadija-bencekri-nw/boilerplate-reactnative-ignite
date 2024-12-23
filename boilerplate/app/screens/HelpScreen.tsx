7a6ad9d6386a6249ab804ade928a56d51017fe55
Khadija <khadija.bencekri@theodo.com>
2024-12-23 09:44:28 +0100

    eslint to Help Screen

--- boilerplate/app/screens/HelpScreen.tsx
+++ boilerplate/app/screens/HelpScreen.tsx
@@ -1,12 +1,18 @@
-import React, { FC } from "react"
-import { observer } from "mobx-react-lite"
-import { ViewStyle } from "react-native"
-import { AppStackScreenProps } from "app/navigators"
+import React from "react"
+
 import { Screen, Text } from "app/components"
+import type { AppStackScreenProps } from "app/navigators"
+import { observer } from "mobx-react-lite"
+import type { FC } from "react"
+import type { ViewStyle } from "react-native"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "app/models"
 
 interface HelpScreenProps extends AppStackScreenProps<"Help"> {}
+
+const $root: ViewStyle = {
+  flex: 1,
+}
 
 export const HelpScreen: FC<HelpScreenProps> = observer(function HelpScreen() {
   // Pull in one of our MST stores
@@ -20,7 +26,3 @@ export const HelpScreen: FC<HelpScreenProps> = observer(function HelpScreen() {
     </Screen>
   )
 })
-
-const $root: ViewStyle = {
-  flex: 1,
-}
