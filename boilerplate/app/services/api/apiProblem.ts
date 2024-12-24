c7d1914aa8e2fb4c31dcdbd043f99f0b240e4371
Khadija <khadija.bencekri@theodo.com>
2024-12-24 17:50:39 +0100

    refactor getGeneralApiProblem and use Record instead of switch

--- boilerplate/app/services/api/apiProblem.ts
+++ boilerplate/app/services/api/apiProblem.ts
@@ -1,4 +1,3 @@
-/* eslint-disable complexity */
 import type { ApiResponse } from "apisauce"
 
 export type GeneralApiProblem =
@@ -48,7 +47,35 @@ export type GeneralApiProblem =
  *
  * @param response The api response.
  */
+
+const apiProblemMapping: Record<string, GeneralApiProblem> = {
+  CONNECTION_ERROR: { kind: "cannot-connect", temporary: true },
+  NETWORK_ERROR: { kind: "cannot-connect", temporary: true },
+  TIMEOUT_ERROR: { kind: "timeout", temporary: true },
+  SERVER_ERROR: { kind: "server" },
+  UNKNOWN_ERROR: { kind: "unknown", temporary: true },
+  CANCEL_ERROR: { kind: "unknown", temporary: true },
+}
+
 export function getGeneralApiProblem(response: ApiResponse<any>): GeneralApiProblem {
+  if (response.problem === "CLIENT_ERROR") {
+    const clientErrorMapping: Record<number, GeneralApiProblem> = {
+      400: { kind: "bad-data" },
+      401: { kind: "unauthorized" },
+      403: { kind: "forbidden" },
+      404: { kind: "not-found" },
+      409: { kind: "user-already-exists" },
+    }
+
+    if (response.status !== undefined) return clientErrorMapping[response.status]
+    return { kind: "rejected" }
+  }
+
+  if (response.problem !== null) return apiProblemMapping[response.problem]
+  return { kind: "unknown", temporary: true }
+}
+
+/* export function getGeneralApiProblem(response: ApiResponse<any>): GeneralApiProblem {
   switch (response.problem) {
     case "CONNECTION_ERROR":
     case "NETWORK_ERROR":
@@ -81,3 +108,4 @@ export function getGeneralApiProblem(response: ApiResponse<any>): GeneralApiProb
       return { kind: "unknown", temporary: true }
   }
 }
+ */
